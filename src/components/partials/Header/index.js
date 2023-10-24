0b9306d6b9c4be432229f09f08d55d9e66183457
Vincent Zerbib <contact@zelabstudio.com>
2023-10-24 15:01:32 +0200

    first commit

---
+++ src/components/partials/Header/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <header className="header__main">
+            <h1 className="logo">My wonderfull app</h1>
+        </header>
+    );
+}
+
+export default Index;
