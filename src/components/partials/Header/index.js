285a93413f4836358e5d3d79ed5d471d1f04c238
Vincent Zerbib <contact@zelabstudio.com>
2023-11-01 17:46:09 +0100

    sa weok

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,9 +1,14 @@
+import SearchInput from "../../UI/SearchInput";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 const Index = () => {
     return (
         <header className={styles.header__main}>
-            <h1 className={styles.logo}>My wonderfull app</h1>
+            <Link href="/">
+                <h1 className={styles.logo}>My wonderfull app</h1>
+            </Link>
+            <SearchInput />
         </header>
     );
 }
